2ed49d283d50b04f8cb5f6634ec46f85855c9e15
NeNeZoRr <zorrillalin@gmail.com>
2023-11-28 13:01:26 -0500

    troubleshooting

--- src/ColorBlock.js
+++ src/ColorBlock.js
@@ -1,26 +1,13 @@
-import React, { useState } from 'react'
-// Make sure to import the component we just built:
-import ColorBlock from './ColorBlock'
+import React from 'react'
 
-function App(){
-    let colors = [
-        'violet', 'blue',
-        'lightblue', 'green',
-        'greenyellow', 'yellow',
-        'orange', 'red'
-    ]
-    
-    let colorMap = colors.map((color, i) => {
-        return (
-            <ColorBlock color={color} />
-        )
-    })
-
+function ColorBlock(props){
     return (
-        <div className="App">
-            {colorMap}
+        <div className="colorBlock"
+        style={{'backgroundColor': props.color}}>
+            <p>{props.color}</p>
         </div>
     )
 }
 
+// Do not forget to export your component once you have built it!
 export default ColorBlock
